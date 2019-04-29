db0b3e3cad9c5785130ea5503fc726a96f45413f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-29 15:21:41 -0700

    wired up event listener

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//selecting the nodes we want to use
+const goodPlaceApp = document.getElementById('good-place-app');
+
+//wiring up form event listener
+
+goodPlaceApp.addEventListener('submit', (event) => {
+    event.preventDefault();   
+    const goodPlaceAppData = new FormData(goodPlaceApp);
+});
