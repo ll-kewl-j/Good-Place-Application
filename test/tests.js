6b38ca70728516c80652d0368df3781e0faceb18
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-29 16:25:57 -0700

    tested TDD and passed

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,57 @@
 const test = QUnit.test;
+function getApplicant(formData) {
+    //convert data into variables
+    const age = parseInt(formData.get('age'));
+    const flossed = formData.get('flossed') === 'yes';
+    console.log(formData.get('flossed'));
+    const wellActually = formData.get('well-actually') === 'yes';
+    const fryCorrection = formData.get('fry-correction') === 'yes';
+    const microwaveFish = parseInt(formData.get('microwave-fish'));
+    
+    //make our object literal
+    const applicant = {
+        name: formData.get('name'),
+        age: age,
+        flossed: flossed,
+        wellActually: wellActually,
+        origin: formData.getAll('origin'),
+        fryCorrection: fryCorrection,
+        favoriteShow: formData.get('favorite-show'),
+        microwaveFish: microwaveFish,
+        whyYou: formData.get('why-you')
+    };
+    return applicant;
+}
 
-test('time to test a function', function(assert) {
+test('creates applicant from form data', (assert) => {
     //Arrange
     // Set up your parameters and expectations
+    const expected = {
+        name: 'Erin Gurley',
+        age: 100,
+        flossed: true,
+        wellActually: true,
+        origin: ['florida'],
+        fryCorrection: true,
+        favoriteShow: 'Duck Dynasty',
+        microwaveFish: 5,
+        whyYou: 'Because I am rad',
+    };
+
+    const formData = new FormData();
+    formData.set('name', expected.name);
+    formData.set('age', expected.age);
+    formData.set('flossed', 'yes');
+    formData.set('well-actually', 'yes');
+    formData.set('origin', expected.origin);
+    formData.set('fry-correction', 'yes');
+    formData.set('favorite-show', expected.favoriteShow);
+    formData.set('microwave-fish', expected.microwaveFish);
+    formData.set('why-you', expected.whyYou);
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const applicant = getApplicant(formData);
     //Assert
-    assert.deepEqual(true, false);
+    assert.deepEqual(applicant, expected);
 });
