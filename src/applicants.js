8303736fe7a6cc930e78b9f9daec2fed355207ad
Erin Gurley <erinmgurley@gmail.com>
2019-05-01 15:30:36 -0700

    created table data and then appended/stick it to the table

---
+++ src/applicants.js
@@ -0,0 +1,36 @@
+//import our utility object
+import applicantApi from './applicant-api.js';
+
+//making our cast list
+const tbody = document.getElementById('applicants');
+
+const applicants = applicantApi.getAll();
+
+//making our for loop to loop through our applicants
+for(let i = 0; i < applicants.length; i++) {
+    const applicant = applicants[i];
+    const tr = document.createElement('tr');
+
+    const nameCell = document.createElement(td);
+    nameCell.textContent = applicant.name;
+    tr.appendChild(nameCell);
+
+    const wellActuallyCell = document.createElement(td);
+    wellActuallyCell.textContent = applicant.wellActually;
+    tr.appendChild(wellActuallyCell);
+
+    const fryCorrectionCell = document.createElement(td);
+    fryCorrectionCell.textContent = applicant.fryCorrection;
+    tr.appendChild(fryCorrectionCell);
+
+    const favoriteShowCell = document.createElement(td);
+    favoriteShowCell.textContent = applicant.favoriteShow;
+    tr.appendChild(favoriteShowCell);
+
+    const microwaveFishCell = document.createElement(td);
+    microwaveFishCell.textContent = applicant.microwaveFish;
+    tr.appendChild(microwaveFishCell);
+
+    tbody.appendChild(tr);
+
+}
