acfcd60f633227aabed1e821f05945c3b416f4ad
Erin Gurley <erinmgurley@gmail.com>
2019-05-02 15:57:46 -0700

    added anchor links to listl

--- src/applicants.js
+++ src/applicants.js
@@ -11,8 +11,26 @@ for(let i = 0; i < applicants.length; i++) {
     const applicant = applicants[i];
     const tr = document.createElement('tr');
 
+    
     const nameCell = document.createElement('td');
-    nameCell.textContent = applicant.name;
+//create the anchor link
+    const link = document.createElement('a');
+    
+//create a search params object 
+    const searchParams = new URLSearchParams();
+    
+//set the name key with the applicant name as the value
+    searchParams.set('name', applicant.name);   
+
+//use the formatted search string as the end of the link href
+    link.href = 'applicant.html?' + searchParams.toString();
+
+//also use the applicant's name as the display text for the link
+    link.textContent = applicant.name;
+    
+//add the link to the name cell
+    nameCell.appendChild(link);
+
     tr.appendChild(nameCell);
 
     const wellActuallyCell = document.createElement('td');
